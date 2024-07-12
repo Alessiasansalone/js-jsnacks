34e25ff8699eb2a56fd474d7285f01b57d82f134
Alessiasansalone <145045809+Alessiasansalone@users.noreply.github.com>
2024-07-12 23:33:27 +0200

    ADD: inizio snack 7

---
+++ JS/snack07.js
@@ -0,0 +1,41 @@
+// Prova funzionamento
+// console.log('07')
+
+// Traccia
+/* 
+Scrivere un programma che stampi la tabellina del 2, fino al numero 1000.
+Modificate poi il programma in modo che venga chiesto all’utente il numero massimo consentito, e stampare tutta la tabellina del 2 fino al numero inserito.
+*/
+
+// Creo un ciclo che stampi i numeri da 1 a 1000
+for (let i = 0; i <= 1000; i++) {
+
+    // Se i è multiplo di 2
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+}
+
+
+
+
+
+
+
+/*
+snack 05
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array. Stampa l’array alla fine.
+
+snack 06
+Fate generare un numero random da 0 a 10 al computer, e chiedete all’utente di inserire un suo numero. Se il numero scelto dall’utente è uguale al numero del computer informate l’utente che ha vinto, altrimenti ha perso.
+
+snack 08
+Fate generare un numero random da 0 a 10 al computer, e chiedete all’utente di inserire un suo numero. Se il numero scelto dall’utente è uguale al numero del computer informate l’utente che ha vinto, altrimenti ha perso.
+A questo punto fate giocare l’utente, fintanto che non vince la partita contro il PC 
+
+snack 10
+Scrivere un programma a cui venga chiesto all’utente di inserire numeri uno dopo l’altro.
+Quando l’utente scriverà 2 volte consecutive lo stesso numero allora il programma dovrà terminare.
+ */
