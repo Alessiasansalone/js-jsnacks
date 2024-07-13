3858ab7d9d6e572f43101009d6f1d4cb34e4b9db
Alessiasansalone <145045809+Alessiasansalone@users.noreply.github.com>
2024-07-13 21:13:51 +0200

    ADD: snack 07 ciclo while

--- JS/snack06.js
+++ JS/snack06.js
@@ -35,15 +35,3 @@ else {
     // Informa l'utente che ha perso
     console.log('Mi dispiace, pensavo al numero ' + randomNum + '. Ritenta! Sarai più fortunato!')
 }
-
-
-
-/*
-snack 08
-Fate generare un numero random da 0 a 10 al computer, e chiedete all’utente di inserire un suo numero. Se il numero scelto dall’utente è uguale al numero del computer informate l’utente che ha vinto, altrimenti ha perso.
-A questo punto fate giocare l’utente, fintanto che non vince la partita contro il PC 
-
-snack 10
-Scrivere un programma a cui venga chiesto all’utente di inserire numeri uno dopo l’altro.
-Quando l’utente scriverà 2 volte consecutive lo stesso numero allora il programma dovrà terminare.
- */
--- JS/snack07.js
+++ JS/snack07.js
@@ -7,12 +7,36 @@ Scrivere un programma che stampi la tabellina del 2, fino al numero 1000.
 Modificate poi il programma in modo che venga chiesto all’utente il numero massimo consentito, e stampare tutta la tabellina del 2 fino al numero inserito.
 */
 
+/* SVOLGIMENTO
 // Dichiaro e assegno una costante in cui chiedo il numero massimo all'utente
 const numMax = parseInt(prompt('Inserisci il numero massimo da stampare per calcolare la tabellina del 2'))
 // Dichiaro e assegno una costante in cui indico che il numIter è numMax / 2
-const numIter = numMax / 2
+const numIter = Math.floor(numMax / 2)
 
 // Creo un ciclo in cui indico al programma che arrivati al numero inserito dall'utente si può fermare
-for (let = i = 0; i <= numIter; i++) {
+for (let i = 0; i <= numIter; i++) {
     console.log('2 * ' + i + ' = ' + (2 * i))
+}
+*/
+
+// CORREZIONE CON CICLO WHILE
+
+// Dichiaro e assegno una costante in cui chiedo il numero massimo all'utente
+const numMax = parseInt(prompt('Inserisci il numero massimo da stampare per calcolare la tabellina del 2'))
+// Dichiaro e assegno una costante in cui indico che il numIter è numMax / 2
+const numIter = Math.floor(numMax / 2)
+
+// Dichiaro il numero da cui parto
+let num = 0
+// Dichiaro un indice
+let i = 0
+
+ 
+while (num < numMax) {
+    // num sarà uguale a i * 2
+    num = i * 2
+    // Stampo la tabellina
+    console.log('2 * ' + i + ' = ' + num)
+    // incremento
+    i++
 }
